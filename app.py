ea154656c162333b82b28fd8969b20ca26d69342
pulnara <magnes.agnieszka@gmail.com>
2018-04-18 12:16:32 +0200

    Reorganize app

--- app.py
+++ app.py
@@ -3,10 +3,27 @@ from converter import RPN_converter
 from simplifier import Quine_McCluskey_simplifier
 import sys
 
-def main():
+def process(expr):
     operators = {'|': 1, '&': 2, '^': 1, '~': 3, '>': 0, '=': 0}
     characters = "".join([chr(i) for i in range(97, 123)]) + "".join([chr(i) for i \
-                    in range(65, 91)]) + "".join(list(map(str, range(10))))
+                                                                      in range(65, 91)]) + "".join(
+        list(map(str, range(10))))
+    try:
+        validator = Validator(operators, characters)
+        expr = validator.validate(expr)
+        converter = RPN_converter(operators, characters)
+        rpn, variables = converter.convert(expr)
+        print("RPN: ", rpn)
+        simplifier = Quine_McCluskey_simplifier(operators, variables, rpn)
+        result = simplifier.simplify()
+        print("Simplified: ", result)
+        return result
+    except ValueError as e:
+        print("Error: " + str(e))
+        exit(1)
+
+def main():
+
     try:
         if len(sys.argv) < 2:
             raise Exception("no expression given")
@@ -14,21 +31,10 @@ def main():
             raise Exception("too many arguments given")
         expr = sys.argv[1]
         print("Input expression: ", expr)
-        validator = Validator(operators, characters)
-        expr = validator.validate(expr)
-        converter = RPN_converter(operators, characters)
-        rpn, variables = converter.convert(expr)
-        print("RPN: ", rpn)
-        simplifier = Quine_McCluskey_simplifier(operators, variables, rpn)
-        simplifier.simplify()
-
+        process(expr)
     except Exception as e:
         print("Error: " + str(e))
         exit(1)
-    except ValueError as e:
-        print("Error: " + str(e))
-        exit(1)
-    #print(expr)
 
 if __name__ == "__main__":
     main()
